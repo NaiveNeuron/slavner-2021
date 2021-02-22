a283318b593a6980c70ae93299d14ae924da8429
Ondrej Jariabka <o.jariabka@gmail.com>
2021-02-22 17:24:04 +0100

    add: stanza tokenizer into preprocess script
    * Added some .gitingore files
    * Added stanza tokenizer into preprocess.py script
    * Added option into proprocess.py to chenge the input path
    * Change README to reflect the changes
    
    Signed-off-by: Ondrej Jariabka <o.jariabka@gmail.com>

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,7 @@ import typer
 
 class Tokenizers(str, Enum):
     nltk = "nltk"
+    stanza = "stanza"
 
 
 def get_nltk_tokenizers() -> Tuple:
@@ -34,12 +35,53 @@ def get_nltk_tokenizers() -> Tuple:
     return word_tokenizer, sent_tokenizer
 
 
+def get_stanza_tokenizers() -> Tuple:
+    import stanza
+
+    cache_tokenizer = None 
+    cache_lang = None
+
+    def _get_tokenizer(lang: str):
+        '''
+        A simple "caching" mechanizsm as loding the tokenizer on each call is
+        extremly expensive.
+        '''
+        nonlocal cache_tokenizer, cache_lang  # not pretty but works
+        if cache_tokenizer is not None and lang == cache_lang:
+            return cache_tokenizer
+        stanza.download(lang)
+        cache_tokenizer = stanza.Pipeline(lang=lang, processors='tokenize')
+        cache_lang = lang
+        return cache_tokenizer
+        
+    def word_tokenizer(text: str, lang: str = 'en') -> List[str]:
+        tokenizer = _get_tokenizer(lang)
+        doc = tokenizer(text)
+        tokens = []
+        for sent in doc.sentences:
+            tokens.extend([token.text for token in sent.tokens if token.text])
+        return tokens
+    
+    def sent_tokenizer(text: str, lang: str = 'en') -> List[str]:
+        tokenizer = _get_tokenizer(lang)
+        doc = tokenizer(text)
+        return [sent.text for sent in doc.sentences]
+
+    return word_tokenizer, sent_tokenizer
+
+
 def main(
     output_path: Path = typer.Argument(
         ...,
         exists=False,
         dir_okay=False,
         readable=True
+    ),
+    input_path: Path = typer.Option(
+        "./bsnlp2021_train_r1/",
+        exists=True,
+        dir_okay=True,
+        file_okay=False
     ),
     tokenizer: Tokenizers = typer.Option(
         Tokenizers.nltk,
@@ -48,9 +90,11 @@ def main(
 ):
     if tokenizer == 'nltk':
         word_tokenizer, sent_tokenizer = get_nltk_tokenizers()
+    elif tokenizer == 'stanza':
+        word_tokenizer, sent_tokenizer = get_stanza_tokenizers()
 
     dataset = SlavNERDataset(
-        data_dir=Path('./bsnlp2021_train_r1/'),
+        data_dir=Path(input_path),
         word_tokenizer=word_tokenizer,
         sent_tokenizer=sent_tokenizer,
     )
