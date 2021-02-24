d764b22010c1c72ca52302d71d79d94fd4f2a6de
Ondrej Jariabka <o.jariabka@gmail.com>
2021-02-24 10:28:24 +0100

    add: generating bio file based on specific column
    * Added a new option to `generate-bio.py` (--split-by) that splits the
    train/validation datasets based on unique values in given column and creates
    files accordingly
    * Added a example of this bahaviour into README
    
    Signed-off-by: Ondrej Jariabka <o.jariabka@gmail.com>

--- generate-bio.py
+++ generate-bio.py
@@ -5,15 +5,15 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def df_to_bio(df: pd.DataFrame,
+def write_bio(df: pd.DataFrame,
               output_column: str,
               output_path: Path,
               separator: str = '\t'):
     with output_path.open('w') as f:
         itertuple = zip(df['words'], df[output_column])
         for words, outputs in tqdm(itertuple,
-                                  total=len(df),
-                                  desc=f"{output_path}"):
+                                   total=len(df),
+                                   desc=f"{output_path}"):
 
             # Covert the strings saved to .csv into Python lists
             words = literal_eval(words)
@@ -22,6 +22,22 @@ def df_to_bio(df: pd.DataFrame,
             for word, out in zip(words, outputs):
                 f.write(word + separator + out + '\n')
             f.write('\n')
+
+
+def df_to_bio(df: pd.DataFrame,
+              output_column: str,
+              output_path: Path,
+              separator: str = '\t',
+              split_by: str = None):
+    if split_by is not None:
+        splits = df.groupby(split_by)
+    else:
+        splits = [("", df)]
+
+    for split_name, df_split in splits:
+        new_filename = output_path.stem + f"_{split_name}" + output_path.suffix
+        split_path = output_path.parent / new_filename
+        write_bio(df_split, output_column, split_path, separator)
 
 
 def main(
@@ -43,6 +59,8 @@ def main(
     ),
     validation_topic: str = typer.Option(...),
     output_column: str = typer.Option(...),
+    delimiter: str = typer.Option('\t'),
+    split_by: str = typer.Option(None),
 ):
     df = pd.read_csv(preprocessed_path)
 
@@ -50,10 +68,10 @@ def main(
     df_val = df[df['topic'] == validation_topic]
 
     typer.echo(f"Writing train data to {train_path}")
-    df_to_bio(df_train, output_column, train_path)
+    df_to_bio(df_train, output_column, train_path, separator=delimiter, split_by=split_by)
 
     typer.echo(f"Writing valid data to {valid_path}")
-    df_to_bio(df_val, output_column, valid_path)
+    df_to_bio(df_val, output_column, valid_path, separator=delimiter, split_by=split_by)
 
 
 if __name__ == '__main__':
