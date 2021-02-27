30fdedce0bbf9643f58df11817db49799ff6b2cf
mr.Shu <mr@shu.io>
2021-02-27 21:59:28 +0100

    trankit: Add the training script
    * Add the first installation of the trankit training script along with
      sacred.
    
    Signed-off-by: mr.Shu <mr@shu.io>

---
+++ trankit/train.py
@@ -0,0 +1,32 @@
+from sacred import Experiment
+import trankit
+
+ex = Experiment()
+
+
+@ex.config
+def config():
+    category = 'customized-ner' # noqa
+    max_epoch = 30 # noqa
+    save_dir = './save_dir' # noqa
+    train_bio_fpath = './train-wo-ryanair.bio' # noqa
+    dev_bio_fpath = './dev-w-ryanair.bio' # noqa
+
+
+@ex.automain
+def main(category, max_epoch, save_dir, train_bio_fpath, dev_bio_fpath):
+
+    # initialize a trainer for the task
+    trainer = trankit.TPipeline(
+        training_config={
+          'max_epoch': max_epoch,
+          'category': category,  # pipeline category
+          'task': 'ner',  # task name
+          'save_dir': save_dir,  # directory to save the trained model
+          'train_bio_fpath': train_bio_fpath,  # training data in BIO format
+          'dev_bio_fpath': dev_bio_fpath  # training data in BIO format
+        }
+    )
+
+    # start training
+    trainer.train()
